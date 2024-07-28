9bd1e802a79d9950384ae71ef3685ae3d92d0388
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-07-29 02:28:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function mincost(arr)
-{ 
+{  let totalCost = 0;
+
+      while (arr.length > 1) {
+       
+		  arr.sort((a, b) => a - b);
+
+          let first = arr.shift();
+        let second = arr.shift();
+
+         let cost = first + second;
+
+        totalCost += cost;
+
+           arr.push(cost);
+    }
+
+    return totalCost;
 //write your code here
 // return the min cost
   
